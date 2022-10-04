bb091083eaa7f36e894c68f647f32e17fc1c122a
leticiavalcan <leticia_eri>
2022-10-04 10:14:47 -0300

    primeira calculadora em js, utilizando functions

---
+++ java script/calc.js
@@ -0,0 +1,26 @@
+const calculate= (number1, number2, operation) =>{
+    let result
+switch (operation){
+    case '+':
+        result= number1 + number2
+        break;
+    case '-':
+        result= number1 - number2
+        break;
+        case '*':
+            result= number1 * number2
+        break;
+        case '/':
+            result= number1 / number2
+            break;
+            default:
+                result= number1 + number2
+                break;
+}
+return result
+}
+
+console.log (calculate(4,7,'+'))
+console.log (calculate(4,7,'-'))
+console.log (calculate(4,7,'*'))
+console.log (calculate(4,7,'/'))
